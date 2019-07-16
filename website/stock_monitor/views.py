288423d0b4ce2d1d8bd4d2f951f46c3b80b656c0
Sean Chen <seanchen0617@gmail.com>
2019-07-16 11:11:10 +0800

    modify view

--- website/stock_monitor/views.py
+++ website/stock_monitor/views.py
@@ -9,24 +9,30 @@ def StockList(request):
 	monitored_stocks = Stocks.objects.all()[:10]
 	result = []
 
+	
 	for _ in monitored_stocks:
-		monitored_stocks_with_price = {}
+		try:
+			monitored_stocks_with_price = {}
 
-		stock = twstock.realtime.get(_.stock_code)
-		
-		monitored_stocks_with_price['stock_name'] = stock['info']['name']
-		monitored_stocks_with_price['stock_code'] = stock['info']['code']
-		monitored_stocks_with_price['stock_open_price'] = stock['realtime']['open']
-		monitored_stocks_with_price['stock_latest_price'] = stock['realtime']['latest_trade_price']
-		monitored_stocks_with_price['percentage'] = round((float(stock['realtime']['latest_trade_price']) - float(stock['realtime']['open']))/float(stock['realtime']['open'])*100, 3)
+			stock = twstock.realtime.get(_.stock_code)
+			
+			monitored_stocks_with_price['stock_name'] = stock['info']['name']
+			monitored_stocks_with_price['stock_code'] = stock['info']['code']
+			monitored_stocks_with_price['stock_open_price'] = stock['realtime']['open']
+			monitored_stocks_with_price['stock_latest_price'] = stock['realtime']['latest_trade_price']
+			monitored_stocks_with_price['percentage'] = round((float(stock['realtime']['latest_trade_price']) - float(stock['realtime']['open']))/float(stock['realtime']['open'])*100, 3)
 
-		stock = twstock.Stock(_.stock_code)
+			stock = twstock.Stock(_.stock_code)
 
-		monitored_stocks_with_price['stock_price'] = ', '.join(map(str,stock.high[-5:]))
+			monitored_stocks_with_price['stock_price'] = ', '.join(map(str,stock.high[-5:]))
+			monitored_stocks_with_price['stock_buy_in_price'] = _.stock_buy_in_price
 
-		result.append(monitored_stocks_with_price)
+			result.append(monitored_stocks_with_price)
 
-		time.sleep(3)
+			time.sleep(3)
+		except TypeError:
+			print("latest_trade_price: %f", stock['realtime']['latest_trade_price'])
+			print("open: %f", stock['realtime']['open'])
 
 	context = {
 		'result': result,
